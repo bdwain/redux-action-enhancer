9bc1b815d348665d2dfdc7c064d80eb0c7e647af
bwain <bwain@grubhub.com>
2016-05-25 18:05:11 -0500

    fix things

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-function createActionEnhancerMiddleware(mappings){
+function createActionEnhancerMiddleware(mappings) {
   return store => next => action => {
     const enhancedFields = mappings.reduce((current, nextMapping) => {
-      if(nextMapping.id in action){
+      if (nextMapping.id in action) {
         return Object.assign(current, nextMapping.mapState(store.getState()));
       }
       return current;
     }, {});
     return next(Object.assign(enhancedFields, action));
-  }
+  };
 }
 
 export default createActionEnhancerMiddleware;
