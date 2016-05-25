6e56369359a24b76da603e72933d557dff8de77a
bwain <bwain@grubhub.com>
2016-05-25 17:42:08 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+function createActionEnhancerMiddleware(mappings){
+  return store => next => action => {
+    const enhancedFields = mappings.reduce((current, nextMapping) => {
+      if(nextMapping.id in action){
+        return Object.assign(current, nextMapping.mapState(store.getState()));
+      }
+      return current;
+    }, {});
+    return next(Object.assign(enhancedFields, action));
+  }
+}
+
+export default createActionEnhancerMiddleware;
