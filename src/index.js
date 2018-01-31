f64dc608c7a5776126b869ce86e14acb2ec05dcb
bwain <bryandwain@gmail.com>
2018-01-30 19:23:23 -0600

    fix symbol polyfill issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function createActionEnhancerMiddleware(getEnhancers) {
   return ({getState}) => next => action => {
     const state = getState();
     const enhancedFields = getEnhancers(state).reduce((current, nextMapping) => {
-      if (nextMapping.id in action) {
+      if (action[nextMapping.id] !== undefined) {
         Object.assign(current, nextMapping.mapState(state, action[nextMapping.id]));
       }
 
