2db307edd97502429dbb620d625affa02e3ee244
bwain <bryandwain@gmail.com>
2020-04-28 11:55:32 -0500

    cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,39 +1,27 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify';
+import { terser } from "rollup-plugin-terser";
 
-var env = process.env.NODE_ENV
+var env = process.env.NODE_ENV;
 var config = {
   output: {
-    format: 'umd'
+    format: 'umd',
+    name: 'ReduxActionEnhancer'
   },
-  name: 'ReduxActionEnhancer',
   plugins: [
-    nodeResolve({
-      jsnext: true
-    }),
+    nodeResolve(),
     babel({
       exclude: 'node_modules/**',
-      plugins: ['external-helpers']
     }),
     replace({
       'process.env.NODE_ENV': JSON.stringify(env)
     })
   ]
-}
+};
 
 if (env === 'production') {
-  config.plugins.push(
-    uglify({
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-        unsafe_comps: true,
-        warnings: false
-      }
-    })
-  )
+  config.plugins.push(terser());
 }
 
-export default config
+export default config;
