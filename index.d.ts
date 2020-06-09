9e4b04302d9963e10142190f7079e76f60760254
bwain <bryandwain@gmail.com>
2020-06-09 15:18:17 -0500

    better types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { Action } from 'redux';
+import { Action, Middleware } from 'redux';
 
 export type Enhancement = object;
 
@@ -10,13 +10,14 @@ type ActionEnhancerById = {
 
 type ActionEnhancerByType = {
   actionTypes: string[];
+};  
+
+type MapState<S, E, V> = {
+  mapState: [V] extends never ? (state: S) => E : (state: S, value: V) => E;
 };
 
-export type ActionEnhancer<S, E extends Enhancement, V = unknown> = (
-  | ActionEnhancerById
-  | ActionEnhancerByType) & {
-  mapState: (state: S, value: V) => E;
-};
+export type ActionEnhancer<S, E extends Enhancement, V = never> =
+  (ActionEnhancerById | ActionEnhancerByType) & MapState<S, E, V>;
 
 export type UnenhancedAction<T> = Action<T> & {
   [index: string]: any;
@@ -34,3 +35,11 @@ export type EnhancedAction<
   E8 extends Enhancement = E1,
   E9 extends Enhancement = E1
 > = A & E1 & E2 & E3 & E4 & E5 & E6 & E7 & E8 & E9;
+
+export type AnyEnhancer<S = unknown> = ActionEnhancer<S, Enhancement, unknown>;
+
+declare function createActionEnhancerMiddleware<S = unknown>(
+  getEnhancers: ((state: S) => AnyEnhancer<S>[]) | AnyEnhancer<S>[] 
+): Middleware;
+
+export default createActionEnhancerMiddleware;
